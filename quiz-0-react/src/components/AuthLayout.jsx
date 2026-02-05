148b614d06aea9e586db850268e0b54e575a7fe6
Why T-hat? <69094042+euvcdoisfilhos@users.noreply.github.com>
2026-02-04 22:55:47 -0300

    incio do layout da pagina de login com componentes react

--- quiz-0-react/src/components/AuthLayout.jsx
+++ quiz-0-react/src/components/AuthLayout.jsx
@@ -1,7 +1,26 @@
 function AuthLayout({ children, title, subtitle }) {
     return (
-        <div className="min-h-screen bg-b-yellow flex items-center justify-center">
+        <div className="min-h-screen flex">
+            {/*Left Panel*/}
+            <div className="bg-neo-yellow w-[50%]">
+                <div className="bg-white border-4">
+                    <div className="bg-black text-white font-bold">
+                        DID YOU KNOW?
+                    </div>
+                    <div>
+                        <div>
+                            <h2>If education itself doesn't change society, then society can't change itself without it.</h2>
+                        </div>
+                        <div>
+                            <p>Paulo Freire</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/*Right Panel*/}
+            <div className="bg-neo-bg w-[50%]">
 
+            </div>
         </div>
     )
 }
