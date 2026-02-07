b302dfc112fb69a67ff8fcba37163c3e907eb5fb
Why T-hat? <69094042+euvcdoisfilhos@users.noreply.github.com>
2026-02-07 20:12:29 -0300

    adicao de rotacao do card da pagina de login

--- quiz-0-react/src/components/AuthLayout.jsx
+++ quiz-0-react/src/components/AuthLayout.jsx
@@ -5,7 +5,7 @@ function AuthLayout({ children, title, subtitle }) {
             {/*Left Panel*/}
 
             <div className="bg-neo-yellow w-full lg:w-1/2 p-5 flex justify-center items-center border-r-5 border-black">
-                <div className="bg-white border-4 p-4 w-[clamp(300px,470px,100%)] h-470px shadow-neo p-5">
+                <div className="bg-white border-4 p-4 w-[clamp(300px,470px,100%)] h-470px shadow-neo p-5 rotate-2 hover:rotate-0 transition-transform duration-300">
                     <div className="bg-black w-40 text-white text-16px p-4 mb-5 ">
                         <h2 className="font-bold">DID YOU KNOW?</h2>
                     </div>
