8b9376bb962190603a5c6e6f271062afcb3d75e8
Why T-hat? <69094042+euvcdoisfilhos@users.noreply.github.com>
2026-02-07 19:49:18 -0300

    Base left pannel finished.

--- quiz-0-react/src/components/AuthLayout.jsx
+++ quiz-0-react/src/components/AuthLayout.jsx
@@ -4,18 +4,18 @@ function AuthLayout({ children, title, subtitle }) {
 
             {/*Left Panel*/}
 
-            <div className="bg-neo-yellow w-full lg:w-1/2 p-4">
-                <div className="bg-white border-4 p-4">
-                    <div className="bg-black w-40 text-white font-bold text-16px font-bold p-4">
-                        DID YOU KNOW?
+            <div className="bg-neo-yellow w-full lg:w-1/2 p-5 flex justify-center items-center border-r-5 border-black">
+                <div className="bg-white border-4 p-4 w-[clamp(300px,470px,100%)] h-470px shadow-neo p-5">
+                    <div className="bg-black w-40 text-white text-16px p-4 mb-5 ">
+                        <h2 className="font-bold">DID YOU KNOW?</h2>
                     </div>
                     <div>
                         <div>
-                            <h2>If education itself doesn't change society, then society can't change itself without it.</h2>
-                            <p>Paulo Freire</p>
+                            <h2 className="font-bold text-2xl mb-5">If education itself doesn't change society, then society can't change itself without it.</h2>
+                            <p className="font-bold text-xl mb-5">Paulo Freire</p>
                         </div>
                     </div>
-                    <img src="src/assets/paulo-freire-removebg-preview.png" alt="Paulo Freire" />
+                    <img src="src/assets/paulo-freire-removebg-preview.png" alt="Paulo Freire" className="mt-[-40px]" />
                 </div>
             </div>
 
