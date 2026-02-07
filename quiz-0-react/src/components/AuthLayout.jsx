3026d6daaa22b652d10f622fac7dc68cbe8967dd
Why T-hat? <69094042+euvcdoisfilhos@users.noreply.github.com>
2026-02-07 11:45:21 -0300

    aplicando css no card da pagina de login

--- quiz-0-react/src/components/AuthLayout.jsx
+++ quiz-0-react/src/components/AuthLayout.jsx
@@ -4,19 +4,18 @@ function AuthLayout({ children, title, subtitle }) {
 
             {/*Left Panel*/}
 
-            <div className="bg-neo-yellow w-full lg:w-1/2">
-                <div className="bg-white border-4">
-                    <div className="bg-black text-white font-bold text-16px font-bold p-4">
+            <div className="bg-neo-yellow w-full lg:w-1/2 p-4">
+                <div className="bg-white border-4 p-4">
+                    <div className="bg-black w-40 text-white font-bold text-16px font-bold p-4">
                         DID YOU KNOW?
                     </div>
                     <div>
                         <div>
                             <h2>If education itself doesn't change society, then society can't change itself without it.</h2>
-                        </div>
-                        <div>
                             <p>Paulo Freire</p>
                         </div>
                     </div>
+                    <img src="src/assets/paulo-freire-removebg-preview.png" alt="Paulo Freire" />
                 </div>
             </div>
 
